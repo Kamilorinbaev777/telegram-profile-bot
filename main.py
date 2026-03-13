25af5707939a6b61e8c759e1feffb0466445dd37
Kamilorinbaev777 <kamilorynbaev@gmail.com>
2026-03-14 01:46:25 +0500

    Update main.py

--- main.py
+++ main.py
@@ -18,32 +18,9 @@ dp.include_router(start_router)
 dp.include_router(register_router)
 dp.include_router(profile_router)
 dp.include_router(fallback_router)
-
-from fastapi import FastAPI, Request
-import uvicorn
-import requests
-import os
-
-WEBHOOK_PATH = f"/{TOKEN}"
-app = FastAPI()
-
-@app.post(WEBHOOK_PATH)
-async def telegram_webhook(req: Request):
-    update = await req.json()
-    print(update)  # handle the update here
-    return {"ok": True}
-
-# optional: set webhook on start
-def set_webhook():
-    url = os.getenv("RAILWAY_STATIC_URL") + WEBHOOK_PATH
-    requests.get(f"https://api.telegram.org/bot{TOKEN}/setWebhook?url={url}")
     
 async def main():
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    set_webhook()
-    import uvicorn
-    port = int(os.getenv("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
     asyncio.run(main())
