6057da05689f7faf8cfb01ebe65af669ac3ac2dd
Kamilorinbaev777 <kamilorynbaev@gmail.com>
2026-03-14 16:22:19 +0500

    Update inline.py

--- keyboards/inline.py
+++ keyboards/inline.py
@@ -4,11 +4,13 @@ front_kb = InlineKeyboardMarkup(
     inline_keyboard=[
         [InlineKeyboardButton(
             text="Register",
-            callback_data='register'
+            callback_data='register',
+            style="primary"
             )],
         [InlineKeyboardButton(
             text="Profile",
-            callback_data='profile'
+            callback_data='profile',
+            style="success"
             )]
         ]
     )
@@ -17,15 +19,18 @@ edit_kb = InlineKeyboardMarkup(
     inline_keyboard=[
         [InlineKeyboardButton(
             text="Name",
-            callback_data='edit_name'
+            callback_data='edit_name',
+            style="primary"
             )],
         [InlineKeyboardButton(
             text="Age",
-            callback_data='edit_age'
+            callback_data='edit_age',
+            style="primary"
             )],
         [InlineKeyboardButton(
             text="Game",
-            callback_data='edit_game'
+            callback_data='edit_game',
+            style="primary"
             )],
         [InlineKeyboardButton(
             text="Back",
@@ -56,7 +61,8 @@ profile_kb1 = InlineKeyboardMarkup(
     inline_keyboard=[
         [InlineKeyboardButton(
             text="Register",
-            callback_data='register'
+            callback_data='register',
+            style="primary"
             )],
         [InlineKeyboardButton(
             text="Back",
@@ -73,7 +79,8 @@ profile_kb2 = InlineKeyboardMarkup(
             )],
         [InlineKeyboardButton(
             text="Edit Profile",
-            callback_data='edit'
+            callback_data='edit',
+            style="primary"
             )],
         [InlineKeyboardButton(
             text="Delete Profile",
@@ -87,7 +94,8 @@ terminate_kb = InlineKeyboardMarkup(
     inline_keyboard=[
         [InlineKeyboardButton(
             text="Terminate",
-            callback_data='terminate'
+            callback_data='terminate',
+            style="danger"
             )]
         ]
     )
